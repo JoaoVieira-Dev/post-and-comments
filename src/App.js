b2b2180c62c3caaf0f11d253ce07d33d9750609f
João Otávio <joaonup.garcia@gmail.com>
2021-01-19 20:11:31 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import  GlobalStyle from './global-styles';
+
+function App() {
+  return (
+      <>
+        <GlobalStyle />
+        <h1>Hello world!</h1>
+      </>
+
+  );
+}
+
+export default App;
